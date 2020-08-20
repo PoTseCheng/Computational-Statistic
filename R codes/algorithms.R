ff19dbda7c802e196257c63ade30d8a5914fd7d0
PoTseCheng <potsecheng@gmail.com>
2020-08-21 00:33:43 +0200

    some more updates

--- R codes/algorithms.R
+++ R codes/algorithms.R
@@ -24,14 +24,26 @@ mytree <- rpart(
 plot(mytree)
 text(mytree, use.n=TRUE, all=TRUE, cex=.8)
 
-printcp(mytree)
-plotcp(mytree)
+mytree2 <- rpart(
+  result ~. , 
+  data = train, 
+  method = "class",
+  control=rpart.control( minsplit=1, minbucket=1, cp=0.001)
+)
+#check what is the most reasonable cp for rpart
+#, minbucket=1
+#severity + gender + age + address + contact + temperature + fever + cough + fatigue + Dyspnea + Headache
+plot(mytree2)
+text(mytree2, use.n=TRUE, all=TRUE, cex=.8)
+
+printcp(mytree2)
+plotcp(mytree2)
 
 mytree3 <- rpart(
   result ~ ., 
   data = train2, 
   method = "class",
-  minsplit = 2, 
+  minsplit = 1, 
   minbucket = 1
  )
 #check what is the most reasonable cp for rpart
@@ -42,11 +54,15 @@ text(mytree3, use.n=TRUE, all=TRUE, cex=.8)
 
 printcp(mytree3)
 plotcp(mytree3)
-#how to test it?
+#weakness, biased towards variables with more variations, which is clear in both 
+#data
+
+#almost uninterpretable when the exogenuous categories are huge
+
+#computational expensive
 
 
 
-#find a way to show the strengh
 #find a way to make the pictures readable
 
 
@@ -59,6 +75,9 @@ J1 <- J48(symptoms ~ .,
           )
 plot(J1)
 summary(J1)
+
+#key difference, post (CART) and pre pruning(c4.5)
+#both rely on information gain formula (with a small twists) 
 #according to the J48 algorithm, the node is not worth splitting
 
 J2 <- J48(result ~.,
@@ -66,7 +85,7 @@ J2 <- J48(result ~.,
           )
 plot(J2)
 summary(J2)
-#resonate with the cp=inf
+#one of the major difference, c4.5 did not choose .blah blah blah
 
 #ct tree by partykit(dont know if we keep it or not)
 
